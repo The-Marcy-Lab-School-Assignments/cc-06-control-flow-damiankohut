7be3be62ca709e20de0b4c3b3fbce5e4ce65044d
damian <damiankohut@gmail.com>
2021-10-12 19:55:48 +0000

    my answer for 1

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 //1
 function sumOfThreeAndFive(){
-  
+  let sum = 0
+  for(let i=1;i < 1000;i++){
+      if (i % 3 === 0 || i % 5 === 0){
+          sum +=i; 
+      }
+  }
+  return sum
+}
+//console.log(sumOfThreeAndFive())
+
+function greaterNum( num1, num2){
+  if(x % 1 === 0 && y % 1 === 0)
+  if (x > y) {
+      return x
+      } else if (y > x} [
+          } else{
+              return "both int are equal"
+          }
+    }else return null
+}
 }
 
-function greaterNum(){
-  
-}
+console.log(10,7)
 
 function oddAndEvenInFifteen(){
   
@@ -14,9 +31,6 @@ function oddAndEvenInFifteen(){
 function sortThreeNums(){
   
 }
-
-
-
 
 
 
